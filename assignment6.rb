d1081362d8018d200c38ef4dabbe93efaf0003b3
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-10-22 11:22:32 -0400

    Questiond 4 asnwered

--- assignment6.rb
+++ assignment6.rb
@@ -8,3 +8,11 @@ grocery_list.each { |item| puts "* #{item}\n" }
 
 #3. 
 p grocery_list.length
+
+#4.
+if 	grocery_list.include?("bananas") == true
+	puts "you need to pick up bananas today"
+else
+	puts "You don't need to pick up bananas today."
+end
+
